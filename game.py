fe7e2a35c17c728a4fe6fd9b63afdcd93604acc8
jamesangie <bucketofpaste@gmail.com>
2020-04-30 22:51:49 -0400

    Updated tower building phase and user interface.

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ attackSpeed = 0
 
 ammoSpeed = 10
 
-a = pygame.Rect(0, 0, 10)
+a = pygame.Rect(0, 0, 600, 50)
 
 
 # Function that checks if object1 with coordinate p1 is in the attack range of object2 with coordinate p2 and range r
@@ -43,10 +43,17 @@ image = pygame.transform.scale(pygame.image.load("venv/image/map/map1.jpg"), (60
 time = pygame.time.Clock()
 ticks = 0
 
-# stats of the enemy, x is initial pos. hp is health
+# stats of the enemy, (x,y) is initial pos. hp is health
 x = -10
 y = 271
 hp = 10
+
+# stats of the tower,
+tower_x = 0
+tower_y = 0
+building = False
+tower_dict = {}
+tower_count = 1
 
 attackTimer = 1
 
@@ -58,13 +65,27 @@ while running:
         running = 0
 
     if event.type == pygame.MOUSEBUTTONDOWN:
+        # when we want to build a tower:
+        if (5 <= pygame.mouse.get_pos()[0] <= 45) and (5 <= pygame.mouse.get_pos()[1] <= 45):
+            # building phase is on, then there is a tower image goes along with the pointer
+            building = True
         print(pygame.mouse.get_pos())
 
-    pygame.draw.rect(screen, 100, a, 1)
+    # when click on good location in building phase, the tower is built.
+    if (building is True) and (event.type == pygame.MOUSEBUTTONDOWN) and (pygame.mouse.get_pos()[1] > 50):
+        # quit building phase
+        building = False
+        # store the tower information in tower_dict
+        tower_dict["tower"+str(tower_count)] = pygame.mouse.get_pos()
+        tower_count += 1
 
     # create the map
     screen.fill((0, 0, 0))
     screen.blit(image, (0, 50))
+
+    # create the building interface
+    pygame.draw.rect(screen, (255, 255, 255, 255), a)
+    screen.blit(pygame.transform.scale(pygame.image.load("venv/image/tower/tower1.jpg"), (40, 40)), (5, 5))
 
     # make the enemy move along the route
     ticks = time.tick(30)
@@ -79,10 +100,16 @@ while running:
     # create enemy base and tower. Enemy can move at a constant speed
     screen.blit(enemy, (x-25, y-25))
     screen.blit(tower, (259, 253))
-    screen.blit(base, (596, 228))
+    screen.blit(base, (596-50, 228-25))
 
     # hit range of the tower
     pygame.draw.circle(screen, 1, (283, 284), 180, 2)
+
+    # when in building phase
+    if building:
+        screen.blit(tower, (pygame.mouse.get_pos()[0]-25, pygame.mouse.get_pos()[1]-25))
+        # shows the hit range of the tower
+        pygame.draw.circle(screen, 1, (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]), 180, 2)
 
     # check if tower can hit the enemy
     if in_range((x, 175), (283, 284), 180):
@@ -93,7 +120,7 @@ while running:
             attackTimer = 1
         # draw ammo
         pygame.draw.rect(screen, 100, pygame.Rect(283-5+(x-283)*attackTimer/ammoSpeed, 284-5+(y-284)
-                                                  * attackTimer / ammoSpeed, 10, 10), 10)
+                                                  * attackTimer / ammoSpeed, 10, 10),10)
         # increase attackTimer so that the ammo can move along the line
         attackTimer += 1
 
