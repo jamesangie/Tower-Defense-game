8e8754b973fedfd6c2c9f894aa6de64489a629bb
jamesangie <bucketofpaste@gmail.com>
2020-05-04 23:44:43 -0400

    Update with multiple enemies(not complete)

--- game.py
+++ game.py
@@ -2,60 +2,61 @@ import pygame
 import math
 import ADP
 
-
+# Initializing global variables
 running = 1
 screen = pygame.display.set_mode((600, 450))
 
-speed = 50
-
 gold = 100
+
+speed = 50
 
 
 # function that move some coordinates at constant speed.
-def move_enemy(ticks):
-    return float(speed) * ticks / 1000
-
-
-attackSpeed = 2
-
-ammoSpeed = 10
-
-a = pygame.Rect(0, 0, 600, 50)
+def move_enemy(ticks, alpha):  # alpha is variable that adjust the speed when there is multiple enemy
+    return float(speed) * ticks * float(alpha) / 1000
 
 
 # Function that checks if object1 with coordinate p1 is in the attack range of object2 with coordinate p2 and range r
 def in_range(p1, p2, r):
-    return math.hypot(p1[0] - p2[0], p1[1] - p2[1]) < 180
+    return math.hypot(p1[0] - p2[0], p1[1] - p2[1]) < r
 
 
+# Attack that reduce enemy's health
 def attack(hp, ticks):
     return hp - float(attackSpeed) * ticks / 1000
 
 
-# a = pygame.Rect(50, 100, 50, 50)
 # create enemy and tower image using the resized pic
 enemy = pygame.transform.scale(pygame.image.load("venv/image/enemy/enemy1.jpg"), (50, 50))
 tower = pygame.transform.scale(pygame.image.load("venv/image/tower/tower1.jpg"), (50, 50))
 base = pygame.transform.scale(pygame.image.load("venv/image/tower/base.jpg"), (50, 50))
 image = pygame.transform.scale(pygame.image.load("venv/image/map/map1.jpg"), (600, 400))
-# introduce time to make the objects move at constant speed
+interface = pygame.Rect(0, 0, 600, 50)
+
+# introducing time makes game running
 time = pygame.time.Clock()
 ticks = 0
 
 # stats of the enemy, (x,y) is initial pos. hp is health
-x = -10
-y = 271
-hp = 10
+x_ini = -10
+y_ini = 271
+hp_ini = 10
+enemy_count = 2
+enemy_dict = {"enemy1": (x_ini, y_ini, hp_ini), "enemy2": (x_ini-100, y_ini, hp_ini)}
+
 
 # stats of the tower,
 tower_x = 0
 tower_y = 0
+attackSpeed = 2
+ammoSpeed = 10
+has_target = False  # If the tower has enemy in range or not
 building = False
 tower_dict = {}
 tower_count = 1
-
 attackTimer = 1
 
+# Game loop
 while running:
     # background image displayed
     event = pygame.event.poll()
@@ -70,30 +71,30 @@ while running:
             building = True
         print(pygame.mouse.get_pos())
 
-
-
     # create the map
     screen.fill((0, 0, 0))
     screen.blit(image, (0, 50))
 
     # create the building interface
-    pygame.draw.rect(screen, (255, 255, 255, 255), a)
-    screen.blit(pygame.transform.scale(pygame.image.load("venv/image/tower/tower1.jpg"), (40, 40)), (5, 5))
-
-    # make the enemy move along the route
+    pygame.draw.rect(screen, (255, 255, 255, 255), interface)
+    screen.blit(pygame.transform.scale(tower, (40, 40)), (5, 5))
     ticks = time.tick(30)
-    if (x < 100 and y == 271) or (y <= 154 and x < 219) or (219 < x < 379 and y >= 305) or (x >= 379 and y < 227):
-        x += move_enemy(ticks)
-    elif (y > 154 and x < 219) or (x > 379 and y < 315):
-        y -= move_enemy(ticks)
-    else:
-        y += move_enemy(ticks)
-    #print(x, y)
 
-    # create enemy and base. Enemy can move at a constant speed
-    screen.blit(enemy, (x-25, y-25))
+    for e in enemy_dict.keys():
+        x = enemy_dict[e][0]
+        y = enemy_dict[e][1]
+        hp = enemy_dict[e][2]
+        # make the enemy move along the route
+        if (x < 100 and y == 271) or (y <= 154 and x < 219) or (219 < x < 379 and y >= 305) or (x >= 379 and y < 227):
+            x += move_enemy(ticks, enemy_count)
+        elif (y > 154 and x < 219) or (x > 379 and y < 315):
+            y -= move_enemy(ticks, enemy_count)
+        else:
+            y += move_enemy(ticks, enemy_count)
+        enemy_dict[e] = x, y, hp
+        # create enemy and base. Enemy can move at a constant speed
+        screen.blit(enemy, (x-25, y-25))
     screen.blit(base, (596-50, 228-25))
-
 
     # when in building phase
     if building:
@@ -105,31 +106,39 @@ while running:
             # quit building phase
             building = False
             # store the tower information in tower_dict
-            tower_dict["tower"+str(tower_count)] = pygame.mouse.get_pos()
+            tower_dict["tower"+str(tower_count)] = pygame.mouse.get_pos()[0]+25, pygame.mouse.get_pos()[1]+25, False
             tower_count += 1
 
+    # let each tower find its enemy and attack
     for t in tower_dict.keys():
-        tx = tower_dict[t][0] - 25
-        ty = tower_dict[t][1] - 25
-        screen.blit(tower, (tx, ty))
+        this_tower = tower_dict[t]
+        tx = this_tower[0]
+        ty = this_tower[1]
+        screen.blit(tower, (tx-25, ty-25))
+        # if the tower doesn't have a enemy to hit now, look for an enemy
+        if not(this_tower[2]):
+            for e in enemy_dict.keys():
+                this_enemy = enemy_dict[e]
+                if in_range((this_enemy[0], this_enemy[1]), (tx+25, ty+25), 180):
+                    tower_dict[t] = (tx, ty, True, e)
+        else:
+            # find it's enemy
+            this_towers_enemy = enemy_dict[this_tower[3]]
 
-        # check if tower can hit the enemy
-        if in_range((x, y), (tx+25, ty+25), 180):
             # attack action has two steps: 1 reduce health, 2 show ammo motion
-            hp = attack(hp, ticks)
+            hp = attack(this_towers_enemy[2], ticks)
             # recreate a ammo when the last hit the target
             if attackTimer == ammoSpeed:
                 attackTimer = 1
             # draw ammo
-            pygame.draw.rect(screen, 100, pygame.Rect(tx+25-5+(x-tx-25)*attackTimer/ammoSpeed, ty+25-5+(y-ty-25) * attackTimer /
+            pygame.draw.rect(screen, 100, pygame.Rect(tx-5+(this_towers_enemy[0]-tx)*attackTimer/ammoSpeed, ty-5+(this_towers_enemy[1]-ty) * attackTimer /
                                                       ammoSpeed, 10, 10), 10)
             # increase attackTimer so that the ammo can move along the line
             attackTimer += 1
 
-            pygame.draw.line(screen, 1, (tx+25, ty+25), (x, y), 2)
-            if hp < 0:
-                print("you win!")
-                running = 0
+            pygame.draw.line(screen, 1, (tx, ty), (this_towers_enemy[0], this_towers_enemy[1]), 2)
+            if hp < 0 or not(in_range((this_towers_enemy[0], this_towers_enemy[1]), (tx+25, ty+25), 180)):
+                tower_dict[t] = (tx, ty, False)
 
     if x > 600:
         print("you lose!")
