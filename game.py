bcb92538733c9b3a97a833b25e6ca1c9d46fb2d9
jamesangie <bucketofpaste@gmail.com>
2020-05-14 00:23:34 -0400

    Fixed bugs. Enemy now can stay dead with no troubles :)

--- game.py
+++ game.py
@@ -43,7 +43,7 @@ y_ini = 271
 hp_ini = 10
 enemy_count = 2
 enemy_dict = {"enemy1": (x_ini, y_ini, hp_ini), "enemy2": (x_ini-100, y_ini, hp_ini)}
-defeated = [        ]
+defeated = []
 
 
 # stats of the tower,
@@ -82,19 +82,20 @@ while running:
     ticks = time.tick(30)
 
     for e in enemy_dict.keys():
-        x = enemy_dict[e][0]
-        y = enemy_dict[e][1]
-        hp = enemy_dict[e][2]
-        # make the enemy move along the route
-        if (x < 100 and y == 271) or (y <= 154 and x < 219) or (219 < x < 379 and y >= 305) or (x >= 379 and y < 227):
-            x += move_enemy(ticks, enemy_count)
-        elif (y > 154 and x < 219) or (x > 379 and y < 315):
-            y -= move_enemy(ticks, enemy_count)
-        else:
-            y += move_enemy(ticks, enemy_count)
-        enemy_dict[e] = x, y, hp
-        # create enemy and base. Enemy can move at a constant speed
-        screen.blit(enemy, (x-25, y-25))
+        if e not in defeated:
+            x = enemy_dict[e][0]
+            y = enemy_dict[e][1]
+            hp = enemy_dict[e][2]
+            # make the enemy move along the route
+            if (x < 100 and y == 271) or (y <= 154 and x < 219) or (219 < x < 379 and y >= 305) or (x >= 379 and y < 227):
+                x += move_enemy(ticks, enemy_count)
+            elif (y > 154 and x < 219) or (x > 379 and y < 315):
+                y -= move_enemy(ticks, enemy_count)
+            else:
+                y += move_enemy(ticks, enemy_count)
+            enemy_dict[e] = x, y, hp
+            # create enemy and base. Enemy can move at a constant speed
+            screen.blit(enemy, (x-25, y-25))
     screen.blit(base, (596-50, 228-25))
 
     # when in building phase
@@ -117,45 +118,44 @@ while running:
         tx = this_tower[0]
         ty = this_tower[1]
         screen.blit(tower, (tx-25, ty-25))
+        if len(this_tower) > 3 and this_tower[3] in defeated:
+            this_tower = (tx, ty, False)
         # if the tower doesn't have a enemy to hit now, look for an enemy
         if not(this_tower[2]):
             for e in enemy_dict.keys():
                 this_enemy = enemy_dict[e]
-                if in_range((this_enemy[0], this_enemy[1]), (tx+25, ty+25), 180):
+                if in_range((this_enemy[0], this_enemy[1]), (tx+25, ty+25), 180) and e not in defeated:
                     tower_dict[t] = (tx, ty, True, e)
         else:
             # find it's enemy
             print(this_tower)
-            this_towers_enemy = enemy_dict[this_tower[3]]
-            hp2 = this_towers_enemy[2]
-            # recreate a ammo when the last hit the target
-            if attackTimer == ammoSpeed:
-                attackTimer = 1
-                # attack action has two steps: 1 reduce health, 2 show ammo motion
-                hp2 = hp2 - 0.7
+            if this_tower[3] not in defeated:
+                this_towers_enemy = enemy_dict[this_tower[3]]
 
-            # draw ammo
-            pygame.draw.rect(screen, 100, pygame.Rect(tx-5+(this_towers_enemy[0]-tx)*attackTimer/ammoSpeed, ty-5+(this_towers_enemy[1]-ty) * attackTimer /
-                                                      ammoSpeed, 10, 10), 10)
-            # increase attackTimer so that the ammo can move along the line
-            attackTimer += 1
+                hp2 = this_towers_enemy[2]
+                # recreate a ammo when the last hit the target
+                if attackTimer == ammoSpeed:
+                    attackTimer = 1
+                    # attack action has two steps: 1 reduce health, 2 show ammo motion
+                    hp2 = hp2 - 0.7
 
-            pygame.draw.line(screen, 1, (tx, ty), (this_towers_enemy[0], this_towers_enemy[1]), 2)
-            # when the enemy is out of range or the enemy is dead, remove the enemy from the dict
-            if hp2 < 0 or not(in_range((this_towers_enemy[0], this_towers_enemy[1]), (tx+25, ty+25), 180)):
-                tower_dict[t] = (tx, ty, False)
-                # if the enemy is dead, remove it from all towers' targets.
-                if hp2 <= 0:
-                    # remove enemy from the dict
-                    del enemy_dict[this_tower[3]]
-                    # remove enemy from all the towers' target
-                    for any_tower in tower_dict.keys():
-                        if tower_dict[any_tower][2] is True:
-                            if tower_dict[any_tower][3] == [this_tower[3]]:
-                                tower_dict[any_tower] = tower_dict[any_tower][0], tower_dict[any_tower][1], False
-            else:
-                enemy_dict[this_tower[3]] = enemy_dict[this_tower[3]][0], enemy_dict[this_tower[3]][1], hp2
+                # draw ammo
+                pygame.draw.rect(screen, 100, pygame.Rect(tx-5+(this_towers_enemy[0]-tx)*attackTimer/ammoSpeed, ty-5+(this_towers_enemy[1]-ty) * attackTimer /
+                                                          ammoSpeed, 10, 10), 10)
+                # increase attackTimer so that the ammo can move along the line
+                attackTimer += 1
 
+                pygame.draw.line(screen, 1, (tx, ty), (this_towers_enemy[0], this_towers_enemy[1]), 2)
+                # when the enemy is out of range or the enemy is dead, remove the enemy from the dict
+                if hp2 < 0 or not(in_range((this_towers_enemy[0], this_towers_enemy[1]), (tx+25, ty+25), 180)):
+                    tower_dict[t] = (tx, ty, False)
+                    # if the enemy is dead, remove it from all towers' targets.
+                    if hp2 <= 0:
+                        # Add defeated enemy to defeated list
+                        defeated += [this_tower[3]]
+
+                else:
+                    enemy_dict[this_tower[3]] = enemy_dict[this_tower[3]][0], enemy_dict[this_tower[3]][1], hp2
 
     if x > 600:
         print("you lose!")
