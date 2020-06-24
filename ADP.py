ff284c02c051a24ce516192a52bf3641cd379250
jamesangie <bucketofpaste@gmail.com>
2020-06-23 22:43:28 -0400

    Update Classes

--- ADP.py
+++ ADP.py
@@ -1,5 +1,5 @@
 import pygame
-
+from Exception import *
 
 class Tower:
     ATTACK_TIMER = 0
@@ -9,6 +9,9 @@ class Tower:
     TOWER_DICT = {"tower1" : [50, 2, 2, 180]}
 
     def __init__(self, name, x, y):
+        """create new tower object by selecting from TOWER_DICT"""
+        if name not in self.TOWER_DICT.keys():
+            raise CantFindTower
         self.name = name
         self.cost = self.TOWER_DICT[name][0]
         self.power = self.TOWER_DICT[name][1]
@@ -30,13 +33,14 @@ class Tower:
 
 
 class Enemy:
-    ENEMY_IMG = pygame.transform.scale(pygame.image.load("venv/image/enemy/enemy1.jpg"), (50, 50))
+    ENEMY_IMG = [pygame.transform.scale(pygame.image.load("venv/image/enemy/enemy1.jpg"), (50, 50))]
     ## name: [speed, hp, gold_drop]
     ENEMY_DICT = {"enemy1": [20, 100, 25]}
 
     def __init__(self, name, x, y):
         self.name = name
         self.speed = self.ENEMY_DICT[name][0]
+        self.img = self.ENEMY_IMG[0]
         self.hp = self.ENEMY_DICT[name][1]
         self.gold_drop = self.ENEMY_DICT[name][2]
         self.x = x
@@ -45,13 +49,29 @@ class Enemy:
     def get_pos(self):
         return self.x, self.y
 
-    def move(self):
+    def move(self, map):
+        for i in map.route:
+
+
+
 
 class Map:
-    MAP_IMG =
-    def __init__(self, name):
-        self.name = name
+    MAP_IMG = ["venv/image/map/map1.jpg", "venv/image/map/map2.jpg"]
+    def __init__(self, map_id, route):
+        self.map_id = map_id
+        self.route = route
+        self.img = self.MAP_IMG[self.map_id-1]
 
+    def extend_route(self, pos):
+        x = pos[0]
+        y = pos[1]
+        last_x = self.route[-1][0]
+        last_y = self.route[-1][1]
+        l = []
+        if abs(x - last_x) >= abs(y - last_y):
+            if x >= last_x:
+                for i in range(last_x, x):
+                    l += [(i+1, min(y, last_y)+(abs(y - last_y))//())]
 
 class Base:
     def __init__(self, name, hp):
