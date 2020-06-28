c2f27dfc1136e7d0fa3eb1f12ed638524ea2084e
jamesangie <bucketofpaste@gmail.com>
2020-06-28 00:37:56 -0400

    Update test cases to fit the changes on Map class

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ADP import *
 
 
 def testEnemyMoving():
-    m = Map(1, [(1, 1),(1, 2),(1, 3),(1, 4)])
+    m = Map("map1", (1, 1),(1, 2),(1, 3),(1, 4))
     enemy = Enemy("enemy1", m.route[0][0], m.route[0][1])
     print("pos of enemy: " + str(enemy.get_pos()) )
     enemy.move(m)
@@ -10,7 +10,7 @@ def testEnemyMoving():
 
 
 def testMapRouteExtend():
-    m = Map(1, [(1, 1)])
+    m = Map("map1", (1, 1))
     print("Route of m: " + str(m.route))
     m.extend_route((2, 2))
     print("Route of m after Extend to (2, 2): " + str(m.route))
@@ -20,11 +20,17 @@ def testMapRouteExtend():
     print("Route of m after Extend to (-9, -9): " + str(m.route))
     m.extend_route((-9, -9))
     print("Route of m after Extend to (-9, -9): " + str(m.route))
-    mm = Map(1, [(1, 1)])
+    mm = Map("map1", (1, 1))
     mm.extend_route((7, 3))
     print("Route of mm after Extend to (7, 3): " + str(mm.route))
     mm.extend_route((9, 9))
     print("Route of mm after Extend to (9, 9): " + str(mm.route))
 
 
-testMapRouteExtend()
+def testMapDrawRoute():
+    m = Map("map1", (1, 1))
+    m.drawRoute()
+
+
+testMapDrawRoute()
+#testMapRouteExtend()
