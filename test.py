a19eecf0d5f999133c4959310242ab53a0b53e39
jamesangie <bucketofpaste@gmail.com>
2020-06-28 16:33:44 -0400

    Update new test cases

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 from ADP import *
 
 
-def testEnemyMoving():
-    m = Map("map1", (1, 1),(1, 2),(1, 3),(1, 4))
-    enemy = Enemy("enemy1", m.route[0][0], m.route[0][1])
-    print("pos of enemy: " + str(enemy.get_pos()) )
-    enemy.move(m)
-    print("pos of enemy after moving 1 time: " + str(enemy.get_pos()))
-
-
 def testMapRouteExtend():
     m = Map("map1", (1, 1))
     print("Route of m: " + str(m.route))
@@ -27,10 +19,39 @@ def testMapRouteExtend():
     print("Route of mm after Extend to (9, 9): " + str(mm.route))
 
 
-def testMapDrawRoute():
+def testEnemyMoving():
     m = Map("map1", (1, 1))
-    m.drawRoute()
+    m.extend_route((7,3))
+    enemy = Enemy("enemy1", m.route[0][0], m.route[0][1])
+    print("pos of enemy: " + str(enemy.get_pos()))
+    enemy.move(m)
+    print("pos of enemy after moving 1 time: " + str(enemy.get_pos()))
 
 
-testMapDrawRoute()
+def testInitializeStatsFile():
+    initialize_stats()
+    with open(os.path.join(stats_path, "object_stats"), 'r') as fhs:
+        s = fhs.read()
+        stats = json.loads(s)
+    print("Default stats file format: "+str(stats))
+
+
+def testUpdateRoute():
+    initialize_stats()
+    updateRoute("map1", [(1, 1), (2, 2), (3, 3)])
+    with open(os.path.join(stats_path, "object_stats"), 'r') as fhs:
+        s = fhs.read()
+        stats = json.loads(s)
+    print("new route: "+str(stats["map"]["map1"]["route"]))
+
+
+def testMapMakeRoute():
+    initialize_stats()
+    m = Map("map1")
+    print(m.route)
+    m.makeRoute()
+
+#testInitializeStatsFile()
+#testUpdateRoute()
+testMapMakeRoute()
 #testMapRouteExtend()
